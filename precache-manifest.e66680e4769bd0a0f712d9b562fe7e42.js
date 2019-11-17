cddaeaa770ab560aceb2c2e8bcb71a916c02500b
Megan Donnelly <md2942@nyu.edu>
2019-11-17 18:20:13 -0500

    Updates

---
+++ precache-manifest.e66680e4769bd0a0f712d9b562fe7e42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3b993a554e31fd1eb1667a5d229cbcb",
+    "url": "/retire-rich/index.html"
+  },
+  {
+    "revision": "53c4f172e8ef8b32cb60",
+    "url": "/retire-rich/static/css/main.0719de10.chunk.css"
+  },
+  {
+    "revision": "1ecc0b93039a714de4bd",
+    "url": "/retire-rich/static/js/2.82935a5a.chunk.js"
+  },
+  {
+    "revision": "53c4f172e8ef8b32cb60",
+    "url": "/retire-rich/static/js/main.3613c655.chunk.js"
+  },
+  {
+    "revision": "e7b34c666b98769762ea",
+    "url": "/retire-rich/static/js/runtime-main.e758657a.js"
+  }
+]);
